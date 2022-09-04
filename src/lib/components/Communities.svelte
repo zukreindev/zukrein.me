9879c7c42700b4ca7383c611f31485694b43993d
zukrein <ardasamedcelik@gmail.com>
2022-09-04 15:37:20 +0300

    Recep ab dedi yaptık

--- src/lib/components/Communities.svelte
+++ src/lib/components/Communities.svelte
@@ -2,16 +2,19 @@
 	let communities = [
 		{
 			name: 'Veiversa Studio',
-			description: 'Veiversa Studio is a fast and original software group. We are passionate about software development and we are always looking for new ideas and technologies. We are a team of creative and talented people who are always ready to help you.',
+			description:
+				'Veiversa Studio is a fast and original software group. We are passionate about software development and we are always looking for new ideas and technologies. We are a team of creative and talented people who are always ready to help you.',
 			image: 'https://cdn.discordapp.com/icons/892759653421809745/824b08bbd98bc8b411a12927a4ffb089.webp',
-			members_length: 4
+			members_length: 4,
+			website: 'https://veiversa.github.io'
 		},
 		{
 			name: 'Vezalan Software',
 			description:
 				'Vezalan Software is a software company with currently 8 members. Most prefered languages among the company is C#, JavaScript and PHP.',
 			image: 'https://cdn.discordapp.com/icons/794275803935932446/055f1ac0e3ae6c851ad06a58c2835836.webp',
-			members_length: 22
+			members_length: 22,
+			website: 'https://vezalansoftware.com'
 		}
 	];
 </script>
@@ -20,14 +23,16 @@
 	<h2 class="text-gray-200 text-4xl my-10 font-semibold">Communities</h2>
 	<div class="flex flex-col items-center space-y-10">
 		{#each communities as community}
-			<div class="bg-[#10151cac] hover:bg-[#10151cc9] transition-all rounded-lg w-full flex p-2 shadow-lg ">
-				<img src={community.image} class="rounded-xl " alt="" />
-				<div class="pl-5 pr-2">
-					<h3 class="text-gray-200 text-xl font-bold py-1">{community.name}</h3>
-					<p class="text-gray-400"> {community.description}</p>
-                    <p class="py-2 text-gray-300">👥 {community.members_length}</p>
-				</div>
-			</div>
+			<a href={community.website} target="_blank" rel="noopener noreferrer">
+				<div class="bg-[#10151cac] hover:bg-[#10151cc9] transition-all rounded-lg w-full flex p-2 shadow-lg ">
+					<img src={community.image} class="rounded-xl " alt="" />
+					<div class="pl-5 pr-2">
+						<h3 class="text-gray-200 text-xl font-bold py-1">{community.name}</h3>
+						<p class="text-gray-400">{community.description}</p>
+						<p class="py-2 text-gray-300">👥 {community.members_length}</p>
+					</div>
+				</div></a
+			>
 		{/each}
 	</div>
 </main>
