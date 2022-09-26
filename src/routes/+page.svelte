2e6a8e572ea25aa4f31b1409c8e8fabcba9709ab
deliever42 <emirhanbaltas34@gmail.com>
2022-09-26 19:26:45 +0300

    remove unused modules

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
 	import About from '$lib/components/About.svelte';
 	import Project from '$lib/components/Project.svelte';
-	import GithubRepo from '$lib/components/GithubRepo.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Communities from '$lib/components/Communities.svelte';
 	import GoHome from '$lib/components/GoHome.svelte';
-
 </script>
 
 <main class="bg-gradient-to-r from-[#1a1d29] to-[#191c24]">
