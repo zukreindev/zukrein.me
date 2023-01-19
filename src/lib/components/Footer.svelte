a7e38f79353ccaa61821b7f64f2dbaa857aa5a4c
ZUKREIN <73394120+zukrein00@users.noreply.github.com>
2023-01-19 22:04:25 +0300

    a
    a

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -10,7 +10,7 @@
 		<div class="space-x-6 flex text-gray-200 text-md ">
 			<a href="http://instagram.com/ardasamedcelik" target="_blank" rel="noopener noreferrer"><h2 class="hover:text-[#3b82f6] transition-colors">Instagram</h2></a>
 			<a href="https://discord.com/users/888795101843820564" target="_blank" rel="noopener noreferrer"><h2 class="hover:text-[#3b82f6] transition-colors">Discord</h2></a>
-			<a href="http://github.com/ardasamedcelik" target="_blank" rel="noopener noreferrer"><h2 class="hover:text-[#3b82f6] transition-colors">GitHub</h2></a>
+			<a href="http://github.com/zukrein00" target="_blank" rel="noopener noreferrer"><h2 class="hover:text-[#3b82f6] transition-colors">GitHub</h2></a>
 		</div>
 	</div>
 </main>
